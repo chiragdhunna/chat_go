5aed5834e4e17da0d0190250b5822f5dae333cfa
chiragdhunna <chiragdhunna2468@gmail.com>
2024-12-11 18:06:52 +0530

    starting setup for state management of user authenticated using redcuer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectRoute from "./components/auth/ProtectRoute";
 import { LayoutLoader } from "./components/layout/Loaders";
+import axios from "axios";
+import { server } from "./constants/config";
+import { useDispatch, useSelector } from "react-redux";
+import { userNotExists } from "./redux/reducers/auth";
 
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
@@ -15,10 +19,21 @@ const ChatManagement = lazy(() => import("./pages/admin/ChatManagement"));
 const UserManagement = lazy(() => import("./pages/admin/UserManagement"));
 const MessageManagement = lazy(() => import("./pages/admin/MessageManagement"));
 
-let user = true;
-
 function App() {
-  return (
+  const { user, loader } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log(server);
+    axios
+      .get(`${server}/api/v1/user/me`)
+      .then((res) => console.log(res))
+      .catch((err) => dispatch(userNotExists()));
+  }, [dispatch]);
+
+  return loader ? (
+    <LayoutLoader />
+  ) : (
     <BrowserRouter>
       <Suspense fallback={<LayoutLoader />}>
         <Routes>
